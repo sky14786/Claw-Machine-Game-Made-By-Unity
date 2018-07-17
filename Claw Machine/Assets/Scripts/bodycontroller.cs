fbadd842b7d1bed7ee9a35e3656641d0a984de68
Kim DongMin <kdm_korea@naver.com>
2018-07-17 20:02:31 +0900

    Refector : Delete overlap and decrease
    Delete overlap to compare inputkey
    
    Decrease to 'if' construction unification

--- Claw Machine/Assets/Scripts/bodycontroller.cs
+++ Claw Machine/Assets/Scripts/bodycontroller.cs
@@ -1,78 +1,58 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BodyController : MonoBehaviour
-{
-    void Start()
-    {
+public class BodyController : MonoBehaviour {
+
+    void Start() { 
         GameManager.instance.MoveSpeed = 0.1f;
     }
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.DownArrow))
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.DownArrow)) {
             moveDown();
-        if (Input.GetKey(KeyCode.UpArrow))
+        }else if (Input.GetKey(KeyCode.UpArrow)) {
             moveUp();
-        if (Input.GetKey(KeyCode.LeftArrow))
+        }else if (Input.GetKey(KeyCode.LeftArrow)) {
             moveLeft();
-        if (Input.GetKey(KeyCode.RightArrow))
+        }else if (Input.GetKey(KeyCode.RightArrow)) {
             moveRight();
+        }
     }
-    void Update()
-    {
+
+    void Update() {
         if (GameManager.instance.PowerTime >= 0)
         {
             GameManager.instance.MoveSpeed = 0.2f;
             GameManager.instance.PowerTime -= Time.deltaTime;
-        }
-        else
+        }else {
             GameManager.instance.MoveSpeed = 0.1f;
-    }
-
-    void moveUp()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (transform.position.y < 1.076795f)
-                gameObject.transform.Translate(0, GameManager.instance.MoveSpeed, 0);
-        }
-    }
-    void moveDown()
-    {
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (transform.position.x > 1.482f && transform.position.y < -1.15f) { }
-            else
-            {
-                if (transform.position.y >= -5.4f)
-                    gameObject.transform.Translate(0, -GameManager.instance.MoveSpeed, 0);
-            }
-        }
-    }
-    void moveLeft()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (transform.position.x > -7.8178f)
-                gameObject.transform.Translate(-GameManager.instance.MoveSpeed, 0, 0);
         }
     }
 
-    void moveRight()
-    {
-        if (Input.GetKey(KeyCode.RightArrow))
+    void moveUp() {
+        if (transform.position.y < 1.076795f) {
+            gameObject.transform.Translate(0, GameManager.instance.MoveSpeed, 0);
+        }
+    }
+
+    void moveDown() {
+        if (transform.position.y >= -5.4f) {
+            gameObject.transform.Translate(0, -GameManager.instance.MoveSpeed, 0);
+        }
+    }
+
+    void moveLeft() {
+        if (transform.position.x > -7.8178f) {
+            gameObject.transform.Translate(-GameManager.instance.MoveSpeed, 0, 0);
+        }
+    }
+
+    void moveRight() {
+        if (transform.position.x < 6.382194f)
         {
-            if (transform.position.x > 0.582197f && transform.position.y < -3.0f && transform.position.x < 2.2f)
-            {
-            }
-            else
-            {
-                if (transform.position.x < 6.382194f)
-                {
-                    gameObject.transform.Translate(GameManager.instance.MoveSpeed, 0, 0);
-                }
-            }
+            gameObject.transform.Translate(GameManager.instance.MoveSpeed, 0, 0);
         }
     }
 }
