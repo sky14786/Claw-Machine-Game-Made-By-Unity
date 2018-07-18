c64678392ab3a39fc86e898495006a4d4d410191
JuneYeoungKim <sky14786@gmail.com>
2018-07-18 10:02:16 +0900

    점수 DB에 삽입시 닉네임값이 null인 경우 익명을 위해 Human + 1~99로 변경

--- Claw Machine/Assets/Scripts/InsertGameScore.cs
+++ Claw Machine/Assets/Scripts/InsertGameScore.cs
@@ -17,6 +17,7 @@ public class InsertGameScore : MonoBehaviour {
         scoretxt.text = ("Your Score : " + GameManager.instance.Score.ToString());
 		isinsertRank = true;
         Panel.active = false;
+        nick_name.text ="Insert Your Nick_name";
     }
 
     public void PanelOn()
@@ -26,6 +27,7 @@ public class InsertGameScore : MonoBehaviour {
     }
     public void Insertscore()
     {
+       
         Debug.Log("점수넣기");
         StartCoroutine(_InsertScore());
         Panel.active = false;
@@ -35,6 +37,8 @@ public class InsertGameScore : MonoBehaviour {
     {
         Debug.Log("Start");
         WWWForm form = new WWWForm();
+        if (nick_name.text == "Insert Your Nick_name")
+           nick_name.text = "Human"+Random.Range(1,99).ToString();
 
         form.AddField("nick_name", nick_name.text);
         form.AddField("score", GameManager.instance.Score);
